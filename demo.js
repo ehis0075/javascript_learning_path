dcc05a0741443f35796133b6beca30ba34f21d34
Ehis <ehisjude420@gmail.com>
2020-09-24 15:32:45 +0100

    data types

--- demo.js
+++ demo.js
@@ -1,1 +1,15 @@
-document.write("Hello ehis...");
+var num = 5; //number
+var name = "ehis";  //string
+var flag = true; //boolean
+ 
+/* 
+js is dynamically typed
+js is loosely typed
+*/    
+num = "jude!!!";
+
+// printing on the browser
+ document.write(name);
+
+ // using alert function
+ alert(num) 
