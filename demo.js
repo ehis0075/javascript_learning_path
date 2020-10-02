7952dda52e540a106f5a44a02bc18b2e6df8049e
Ehis <ehisjude420@gmail.com>
2020-10-02 17:01:23 +0100

    control statement

--- demo.js
+++ demo.js
@@ -25,34 +25,60 @@ js is loosely typed
 //conditional statement
 
 // USING IF ELSE STATEMENT
-var c = 6;
-if(c%2==0)
+// var c = 6;
+// if(c%2==0)
+// {
+// document.write("its an even number")
+// }
+// else
+// {
+//     document.write("old number");
+// }
+
+
+// // switch case
+// var day = 1;
+
+// switch(day)
+// {
+//     case 1:
+//         document.write("<h2>Sunday</h2>");
+//         break;
+//     case 2:
+//         document.write("<h2>Monday</h2>");
+//         break;
+//     case 3:
+//         document.write("<h2>Tuesday</h2>");
+//         break;                    
+//     default:
+//         document.write("<h2>wrong input</h2>");
+// }
+
+
+// control statement
+//1 for loops : is used when the number of iteration is known in advanced
+
+for(var x=1;x<=10;x++)
 {
-document.write("its an even number")
+    document.write("<h2>"+(5*x)+"</h2>");
 }
-else
+
+
+//2 while loop 
+// usee when you dont know the number of iteration
+
+var x=1;
+while(x<=10)
 {
-    document.write("old number");
+document.write("<h1>"+x+"</h2>");
+x=x+2;
 }
 
+//3 Do while loop
+// here the condition is checked after the first iteration 
+var x = 7;
+do {
+    document.write("<h2>ehis</h2>");
+    x--;
+} while(x>5);
 
-// switch case
-var day = 1;
-
-switch(day)
-{
-    case 1:
-        document.write("<h2>Sunday</h2>");
-        break;
-    case 2:
-        document.write("<h2>Monday</h2>");
-        break;
-    case 3:
-        document.write("<h2>Tuesday</h2>");
-        break;                    
-    default:
-        document.write("<h2>wrong input</h2>");
-}
-
-
-// for loops
