82cffbc2a33458c2575b6ab4d59daa21d9d7506c
Ehis <ehisjude420@gmail.com>
2020-10-02 09:54:36 +0100

    using switch case

--- demo.js
+++ demo.js
@@ -1,15 +1,58 @@
-var num = 5; //number
-var name = "ehis";  //string
-var flag = true; //boolean
+// var num = 5; //number
+// var name = "ehis";  //string
+// var flag = true; //boolean
  
 /* 
 js is dynamically typed
 js is loosely typed
 */    
-num = "jude!!!";
+//num = "jude!!!";
 
 // printing on the browser
- document.write(name);
+ //document.write(name);
 
  // using alert function
- alert(num) 
+ //alert(num) 
+
+
+
+
+
+ 
+ //Operators
+// 
+
+//conditional statement
+
+// USING IF ELSE STATEMENT
+var c = 6;
+if(c%2==0)
+{
+document.write("its an even number")
+}
+else
+{
+    document.write("old number");
+}
+
+
+// switch case
+var day = 1;
+
+switch(day)
+{
+    case 1:
+        document.write("<h2>Sunday</h2>");
+        break;
+    case 2:
+        document.write("<h2>Monday</h2>");
+        break;
+    case 3:
+        document.write("<h2>Tuesday</h2>");
+        break;                    
+    default:
+        document.write("<h2>wrong input</h2>");
+}
+
+
+// for loops
